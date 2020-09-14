18cae1fbbd3cf362ac12575eaee096fc765ac62c
SilentiumArt <71014266+SilentiumArt@users.noreply.github.com>
2020-09-14 15:39:29 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+// function showSocial(event) { 
+//     event.preventDefault(); 
+//     const modal = document.querySelector('.modal');
+//     const arrow = document.querySelector('.arrow-down');
+//     modal.classList.toggle("d-none");
+//     arrow.classList.toggle("rotate");
+//  }
+
+$(document).ready(function () {
+    $('.showSocial').click(function (e) { 
+        e.preventDefault();
+        $('.modal').toggleClass('d-none');
+        $('.arrow-down').toggleClass('rotate')
+    });
+    $('.main-screen, .arrow-up').click(function (e) { 
+        e.preventDefault();
+        $('.modal').addClass('d-none');
+        $('.arrow-down').removeClass('rotate')
+    });
+    $('.showBIO').click(function (e) { 
+        e.preventDefault();
+        $('.modal-bio').fadeToggle();
+    });
+});
+
+// Ниже: скрипты, которые по нажатию на кнопку в галерее скрывают галерею и открывают тематический раздел
+
+$('.landingButton').click(function (e) { 
+    e.preventDefault();
+    $('.gallery-list, .gallery .section_head>span').fadeOut();
+    $('.landing-wrap').fadeIn();
+    $('.gallery .section_head>h2').text('ЛЕНДИНГ');
+    $('.backToGallery').fadeIn();
+});
+$('.comicsButton').click(function (e) { 
+    e.preventDefault();
+    $('.gallery-list, .gallery .section_head>span').fadeOut();
+    $('.comics-wrap').fadeIn();
+    $('.gallery .section_head>h2').text('КОМИКСЫ');
+    $('.backToGallery').fadeIn();
+});
+$('.artButton').click(function (e) { 
+    e.preventDefault();
+    $('.gallery-list, .gallery .section_head>span').fadeOut();
+    $('.art-wrap').fadeIn();
+    $('.gallery .section_head>h2').text('АРТ');
+    $('.backToGallery').fadeIn();
+});
+$('.sketchButton').click(function (e) { 
+    e.preventDefault();
+    $('.gallery-list, .gallery .section_head>span').fadeOut();
+    $('.sketch-wrap').fadeIn();
+    $('.gallery .section_head>h2').text('СКЕТЧ');
+    $('.backToGallery').fadeIn();
+});
+$('.portretButton').click(function (e) { 
+    e.preventDefault();
+    $('.gallery-list, .gallery .section_head>span').fadeOut();
+    $('.portret-wrap').fadeIn();
+    $('.gallery .section_head>h2').text('ПОРТРЕТ');
+    $('.backToGallery').fadeIn();
+});
+
+// Здесь заканчиваются скрипты с разкрытием тематических разделов.
+// Ниже: скрипт, который по нажатию на кнопку "ГАЛЕРЕЯ" в тематическом разделе галереи скрывает его и снова показывает главную галереи
+
+$('.backToGallery').click(function (e) { 
+    $('.typeOfGallery-wrap').fadeOut();
+    $('.gallery-list, .gallery .section_head>span').fadeIn();
+    $('.gallery .section_head>h2').text('ГАЛЕРЕЯ');
+    $(this).fadeOut(); 
+});
+
+
+
